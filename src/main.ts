5f1610ecc536fe69236de771d8465560c64ce135
Angel Gomez <106106151+AngelGomez12@users.noreply.github.com>
2025-10-27 20:52:29 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+import { AppModule } from "@/app/app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.setGlobalPrefix("api");
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>("PORT", "3000");
+
+  const config = new DocumentBuilder()
+    .setTitle("Corna App")
+    .setDescription("The Corna App API description")
+    .setVersion("1.0")
+    .addTag("corna")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+  await app.listen(port, "0.0.0.0");
+
+  const logger = app.get(Logger);
+  logger.log(`App is ready and listening on port ${port} 🚀`);
+}
+
+bootstrap().catch(handleError);
+
+function handleError(error: unknown) {
+  // eslint-disable-next-line no-console
+  console.error(error);
+  // eslint-disable-next-line unicorn/no-process-exit
+  process.exit(1);
+}
+
+process.on("uncaughtException", handleError);
